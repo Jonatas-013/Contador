65aab6fa5d791af42459b5711f7a0fd630eaf9c5
Jonatas <jonatas_013@yahoo.com.br>
2022-05-30 17:22:42 -0300

    contador

---
+++ README.md
@@ -0,0 +1,4 @@
+Olá pessoal.
+
+
+Seguindo os estudo de JS, segue um contador bem simples. Fortalecendo os conceitos até aqui aprendidos.
---
+++ script.js
@@ -0,0 +1,13 @@
+var numberatualspam = document.getElementById('valor')
+var valor = 0
+
+
+function increment() {
+    valor = valor + 1
+    numberatualspam.innerHTML = valor
+}
+
+function decrement() {
+    valor = valor - 1
+    numberatualspam.innerHTML = valor
+}
